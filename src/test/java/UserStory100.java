cd56ff3f30a122253a70a17fc9a423dac2b287fd
Ahmad Seyar Sabawoon <seyarkhan143@gmail.com>
2021-06-30 16:47:55 -0700

    User Story work 2

--- src/test/java/UserStory100.java
+++ src/test/java/UserStory100.java
@@ -3,5 +3,6 @@ public class UserStory100 {
     public static void main(String[] args) {
         System.out.println("I love to practice coding in my free time");
         // work 1
+        // work 2
     }
 }
