01382f924375727d85077284e02bc96bc1e1779e
Ahmad Seyar Sabawoon <seyarkhan143@gmail.com>
2021-06-30 17:25:41 -0700

    User Story 101 has been completed

--- src/test/java/UserStory101.java
+++ src/test/java/UserStory101.java
@@ -1,3 +1,8 @@
 public class UserStory101 {
+    public static void main(String[] args) {
 
+
+        // work 1 has been done here, and ready to push remote
+        // so i can merger to master
+    }
 }
